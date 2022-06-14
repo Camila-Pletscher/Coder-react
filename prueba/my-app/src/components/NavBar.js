dc139138184b6dddaec4743903320336a308cc9b
Cami <camipletscher@gmail.com>
2022-06-14 01:13:39 -0300

    Empece a dar estilo a mi app

---
+++ prueba/my-app/src/components/NavBar.js
@@ -0,0 +1,23 @@
+import './NavBar.css';
+
+import 'materialize-css/dist/css/materialize.min.css';
+
+function NavBar() {
+    return (
+        <nav>
+    <div className="nav-wrapper">
+      <a href="#" className="brand-logo">
+        <img src=""></img>
+      </a>
+      <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <li><a href="sass.html">Remeras</a></li>
+        <li><a href="badges.html">Buzos</a></li>
+        <li><a href="collapsible.html">Contacto</a></li>
+      </ul>
+    </div>
+  </nav>
+    );
+  }
+  
+  export default NavBar;
+
