5e3c189e97076d8d30c8bf4f777799f4309f19ef
Cami <camipletscher@gmail.com>
2022-06-15 11:19:28 -0300

    agrego nav

--- prueba/my-app/src/components/NavBar.js
+++ prueba/my-app/src/components/NavBar.js
@@ -6,8 +6,8 @@ function NavBar() {
     return (
         <nav>
     <div className="nav-wrapper">
-      <a href="#" className="brand-logo">
-        <img src=""></img>
+      <a href="#" className="brand-logo">        
+        <i className='large material-icons'>Moon</i>
       </a>
       <ul id="nav-mobile" className="right hide-on-med-and-down">
         <li><a href="sass.html">Remeras</a></li>
