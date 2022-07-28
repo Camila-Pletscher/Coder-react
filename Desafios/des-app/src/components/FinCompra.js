3a353a4a141eb8ef77c18a162fc069863e67171c
Cami <camipletscher@gmail.com>
2022-07-28 01:47:57 -0300

    Formulario a firebase resuelto

--- Desafios/des-app/src/components/FinCompra.js
+++ Desafios/des-app/src/components/FinCompra.js
@@ -1,19 +1,24 @@
 import { addDoc, collection, getFirestore } from "firebase/firestore";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { CartContext } from "../context/CartContext";
 
 
 function FinCompra(){
 
   const {cartItems} = useContext(CartContext);
-  console.log(cartItems);
+
+  const [name, setName] = useState('');
+
+  const [email, setEmail] = useState('');
+
+  const [phone, setCell] = useState('');
 
   
 
   const sendOrder = () => {
     const order = {
-          buyer: { name: "agus", phone: "111", email: "a@A.com" },
-          items: [...cartItems],
+          buyer: { name: name, phone: phone, email: email },
+          items: [{...cartItems}],
           total: 100, 
     }
 
@@ -24,13 +29,31 @@ function FinCompra(){
     addDoc(ordersCollection, order).then(({id}) =>console.log(id))
   }
 
+  const cashOutSubmit = (e) => {
+    e.preventDefault();
+    sendOrder();
+    setName('');
+    setEmail('');
+    setCell('');
+  }
+
   
       
     
       
 
       return(
-        <button onClick={sendOrder}>Finalizar compra</button>
+        <form onSubmit={cashOutSubmit}>
+          <label>Name</label>
+          <input type="text" onChange={(e)=>setName(e.target.value)} value={name} />
+          <label>Email</label>
+          <input type="email" onChange={(e)=>setEmail(e.target.value)}  value={email} />
+          <label>Phone</label>
+          <input type="number" onChange={(e)=>setCell(e.target.value)}  value={phone} />
+
+          <button onClick={sendOrder}>Finalizar compra</button>
+        </form>
+        
       )
 }
 
